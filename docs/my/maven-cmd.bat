0d96518652057a04a966c1664db4f0db8da27dc4
ecloud <wuajohn@126.com>
2025-02-04 10:51:34 +0800

    编译

---
+++ docs/my/maven-cmd.bat
@@ -0,0 +1,39 @@
+set JAVA_HOME=D:\app\jdk-dragonwell-17.0.11.0.12
+set PATH=%JAVA_HOME%\bin;%PATH%
+
+cd /d D:\project\z-opensource\hop
+
+mvn clean install
+
+mvn -rf :hop-core install
+
+mvn -rf :hop-core install -DskipTests
+
+
+mvn install:install-file -Dfile=D:\var\tmp\commons-validator-1.9.0.jar -DgroupId=commons-validator -DartifactId=commons-validator -Dversion=1.9.0 -Dpackaging=jar
+
+
+mvn install -rf :hop-engines-beam
+
+
+
+find /cygdrive/d/app/maven/ -type f -mtime -7 -exec zip -r /cygdrive/d/var/tmp/maven-20250120.zip {} +
+
+
+
+
+
+
+$recentFiles = Get-ChildItem -Path "D:\app\maven\" -Recurse | Where-Object { $_.LastWriteTime -gt (Get-Date).AddDays(-7) }
+Compress-Archive -Path $recentFiles.FullName -DestinationPath "D:\var\tmp\maven-20250120.zip"
+
+
+git checkout -b release/2.11.0
+git push --set-upstream origin release/2.11.0
+
+
+apache/hop:release/2.11.0
+git@github.com:wu3ajohn/hop.git
+
+
+2.12.0-SNAPSHOT ->2.11.0 
